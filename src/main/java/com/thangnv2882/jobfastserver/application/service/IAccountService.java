e7b7c0ce5269bfabdb90d7b5c441bd3a19c123c3
Nguyen Van Thang <thang.nv2882@gmail.com>
2022-12-29 22:17:24 +0700

    [update] change the location of FindAccountInput

--- src/main/java/com/thangnv2882/jobfastserver/application/service/IAccountService.java
+++ src/main/java/com/thangnv2882/jobfastserver/application/service/IAccountService.java
@@ -3,7 +3,7 @@ package com.thangnv2882.jobfastserver.application.service;
 import com.thangnv2882.jobfastserver.application.input.account.ChangeAvatarInput;
 import com.thangnv2882.jobfastserver.application.input.account.GetAccountByEmailInput;
 import com.thangnv2882.jobfastserver.application.input.account.UpdateAccountInput;
-import com.thangnv2882.jobfastserver.application.input.commons.FindAccountInput;
+import com.thangnv2882.jobfastserver.application.input.account.FindAccountInput;
 import com.thangnv2882.jobfastserver.application.input.commons.Input;
 import com.thangnv2882.jobfastserver.application.output.account.GetAccountOutput;
 import com.thangnv2882.jobfastserver.application.output.account.GetListAccountOutput;
